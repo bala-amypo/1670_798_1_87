eed5fea89aaf197f9c0427579ff067af382dc6c2
bala-amypo <bala@amypo.in>
2025-12-24 16:42:47 +0000

    User commit

--- src/main/java/com/example/demo/dto/ActivityLogRequest.java
+++ src/main/java/com/example/demo/dto/ActivityLogRequest.java
@@ -3,12 +3,30 @@ package com.example.demo.dto;
 import java.time.LocalDate;
 
 public class ActivityLogRequest {
+
     private Double quantity;
     private LocalDate activityDate;
 
-    public Double getQuantity() { return quantity; }
-    public void setQuantity(Double quantity) { this.quantity = quantity; }
+    public ActivityLogRequest() {}
 
-    public LocalDate getActivityDate() { return activityDate; }
-    public void setActivityDate(LocalDate activityDate) { this.activityDate = activityDate; }
+    public ActivityLogRequest(Double quantity, LocalDate activityDate) {
+        this.quantity = quantity;
+        this.activityDate = activityDate;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getActivityDate() {
+        return activityDate;
+    }
+
+    public void setActivityDate(LocalDate activityDate) {
+        this.activityDate = activityDate;
+    }
 }
