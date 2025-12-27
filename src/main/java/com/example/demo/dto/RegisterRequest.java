48435e1e40ae6adf17dc98b735a06957a21de603
bala-amypo <bala@amypo.in>
2025-12-27 15:16:35 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,7 +1,7 @@
 package com.example.demo.dto;
 
 public class RegisterRequest {
-    private String name;  // Make sure it's "name" not "full_name"
+    private String name;
     private String email;
     private String password;
 
